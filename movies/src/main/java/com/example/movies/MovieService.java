b764e9afc29385fb2e692a45dc3fcfd5abff79df
NavyaSri-2001 <80478150+NavyaSri-2001@users.noreply.github.com>
2024-04-12 00:36:32 +0530

    Add files via upload
    Backend code commit

---
+++ movies/src/main/java/com/example/movies/MovieService.java
@@ -0,0 +1,24 @@
+package com.example.movies;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        System.out.println(repository.findMovieByImdbId(imdbId));
+        return repository.findMovieByImdbId(imdbId);
+    }
+
+}
